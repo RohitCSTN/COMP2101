f75b09f6dbbfb95117fa330f0fccff9c6bcd6e8e
RohitCSTN <105671733+RohitCSTN@users.noreply.github.com>
2022-08-16 19:15:56 -0400

    Add files via upload

---
+++ Screenshots powershell/Lab 5.ps1
@@ -0,0 +1,33 @@
+﻿Param ([Parameter][string]$System,
+       [Parameter][string]$Disks,
+       [Parameter][string]$Network)
+
+Write-Output "computer info"
+if ($Disks -ne $true -and $Disks -ne $true -and $Network -ne $true) {
+    systeminfo
+    operating_system
+    Processor
+    Memory
+    disk
+    Networkadapter
+    videoresolution
+}
+
+Write-Output "disk report"
+if($Disks -eq 'true' -Or $Disks -ne 'true'){
+disk
+}
+
+Write-Output "Network info"
+if($Network -eq 'true'-Or $Network -ne 'true'){
+Networkadapter
+}
+
+if ($System -eq $true) {
+    systeminfo
+    operating_system
+    Processor
+    Memory
+    videoresolution
+}
+
